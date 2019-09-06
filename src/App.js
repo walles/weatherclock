e9152a35aabda6f354fa524d3050b0ad4c5176a8
Johan Walles <johan.walles@gmail.com>
2019-09-06 19:36:54 +0200

    Do top level page
    Mostly.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Imagine a clock face here</p>
+
+      {/*
+      If you add a Weatherclock launcher to your home screen on an iPhone,
+      the page opened will not be in a web-browser (or at least look like
+      it's not).
+
+      So we add a reload button of our own here.
+      */}
+      <button type="button" onclick="location.reload();">Update forecast FIXME: Handler doesnt work</button>
+
+      <p>Weather forecast from <a href="yr.no">yr.no</a>, delivered by the
+      <a href="http://met.no/English/">Norwegian Meteorological Institute</a>
+      and the <a href="http://www.nrk.no/">NRK</a>.</p>
+
+      <p>Imagine a share-on-Facebook button here</p>
+
+      <p>
+        <a href="https://github.com/walles/weatherclock">Source code on GitHub</a>
+      </p>
     </div>
   );
 }
