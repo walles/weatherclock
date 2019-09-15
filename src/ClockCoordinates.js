cab15f346844ccea3659d0779323411359a5cd8d
Johan Walles <johan.walles@gmail.com>
2019-09-15 15:26:27 +0200

    Place displays properly

--- src/ClockCoordinates.js
+++ src/ClockCoordinates.js
@@ -44,14 +44,44 @@ class ClockCoordinates {
     return this.decimalHour < 7 || this.decimalHour > 20
   }
 
+  _degreesDistance = (d0, d1) => {
+    const distance = Math.abs(d1 - d0)
+    if (distance > 180) {
+      return 360 - distance
+    }
+    return distance
+  }
+
   /**
    * Return an array of directions (0-360 degrees). The first direction
    * is the one that is most clear of the clock hands, second one is second
    * most clear and so on.
    */
   rankFreeDirections = () => {
-    // FIXME: Actually figure this out properly
-    return [90, 180]
+    const degreeCandidates = [0, 90, 180, 270]
+    const minuteDegrees = 360 * (this.decimalHour % 1.0)
+    const hourDegrees = 360 * ((this.decimalHour % 12.0) / 12.0)
+
+    let degreesToRank = {}
+    degreeCandidates.forEach(degrees => {
+      const minDistance = this._degreesDistance(degrees, minuteDegrees)
+      const hourDistance = this._degreesDistance(degrees, hourDegrees)
+      degreesToRank[degrees] = Math.min(minDistance, hourDistance)
+    })
+
+    degreeCandidates.sort((degree1, degree2) => {
+      const rank1 = degreesToRank[degree1]
+      const rank2 = degreesToRank[degree2]
+      if (rank1 < rank2) {
+        return 1
+      }
+      if (rank1 > rank2) {
+        return -1
+      }
+      return 0
+    })
+
+    return degreeCandidates
   }
 }
 
