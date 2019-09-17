acad15ccbe818a388f8068d1e4de60a213eba556
Johan Walles <johan.walles@gmail.com>
2019-09-17 14:08:03 +0200

    Updates

---
+++ precache-manifest.aa23635089b3457dec3dc6800fcbfb2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fdb2aa6e63f6c0ffb3656bd90fc8ef1",
+    "url": "/weatherclock/index.html"
+  },
+  {
+    "revision": "fbf1f83ab6795e90cc82",
+    "url": "/weatherclock/static/css/main.b8352cf3.chunk.css"
+  },
+  {
+    "revision": "f2d7fc83f69d497f8b41",
+    "url": "/weatherclock/static/js/2.3d94949b.chunk.js"
+  },
+  {
+    "revision": "fbf1f83ab6795e90cc82",
+    "url": "/weatherclock/static/js/main.771819d7.chunk.js"
+  },
+  {
+    "revision": "f483ed314d0e8c767566",
+    "url": "/weatherclock/static/js/runtime~main.c47a92ec.js"
+  }
+]);
