00abec933dbb21e4220c87e9cd4ef413fa0af88a
Johan Walles <johan.walles@gmail.com>
2019-09-21 17:07:44 +0200

    Move time selector to top right corner of the page

--- src/TimeSelect.js
+++ src/TimeSelect.js
@@ -5,9 +5,11 @@ import NativeSelect from '@material-ui/core/NativeSelect'
 
 class TimeSelect extends React.Component {
   render = () => {
+    const topRight = { position: 'absolute', right: '0px', top: '0px' }
+
     // Inspired by: https://material-ui.com/components/selects/#native-select
     return (
-      <NativeSelect value={'now'}>
+      <NativeSelect style={topRight} value={'now'}>
         <option value={'now'}>Now</option>
         <option value={'tomorrow'}>Tomorrow</option>
       </NativeSelect>
