43d17915450f11cb928cbba68a7c08f7b1f31c87
Johan Walles <johan.walles@gmail.com>
2019-09-21 17:26:10 +0200

    Switch dropdown value on selections

--- src/TimeSelect.js
+++ src/TimeSelect.js
@@ -4,16 +4,30 @@ import PropTypes from 'prop-types'
 import NativeSelect from '@material-ui/core/NativeSelect'
 
 class TimeSelect extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      value: 'now'
+    }
+  }
+
   render = () => {
     const topRight = { position: 'absolute', right: '0px', top: '0px' }
 
     // Inspired by: https://material-ui.com/components/selects/#native-select
     return (
-      <NativeSelect style={topRight} value={'now'}>
+      <NativeSelect style={topRight} value={this.state.value} onChange={this.onChange}>
         <option value={'now'}>Now</option>
         <option value={'tomorrow'}>Tomorrow</option>
       </NativeSelect>
     )
+  }
+
+  onChange = event => {
+    this.setState({ value: event.target.value })
+
+    // FIXME: Report to this.props.onSelect!
   }
 }
 
