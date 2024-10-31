255f4b6919e7c55cc2ed0da463b5659812cde4fe
Johan Walles <johan.walles@gmail.com>
2024-10-31 18:18:00 +0100

    Update a comment

--- src/Forecast.ts
+++ src/Forecast.ts
@@ -3,6 +3,6 @@ export type Forecast = {
   span_h: number; // Width of the span in hours
   celsius?: number; // The forecasted temperatures in centigrades
   wind_m_s?: number; // The forecasted wind speed in m/s
-  symbol_code?: string; // The weather symbol code. Resolve using public/api-met-no-weathericons/png/SYMBOL_CODE.png
+  symbol_code?: string; // The weather symbol code. Example values: 'clearsky_day', 'heavysnow'
   precipitation_mm?: number;
 };
