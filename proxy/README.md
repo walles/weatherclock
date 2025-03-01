fd88b894c77969c28248894ef1835d91533860f9
Johan Walles <johan.walles@gmail.com>
2025-03-01 08:53:23 +0100

    Fix deployment instructions

--- proxy/README.md
+++ proxy/README.md
@@ -1,5 +1,6 @@
 CORS proxy for requests to <api.met.no> from the Weather Clock.
 
-Deployed as a Google Cloud Function.
+Deployed as a Google Cloud Function:
+https://console.cloud.google.com/run/detail/europe-north1/api-met-no-proxy/source?inv=1&invt=Abq2CA&project=api-met-no-proxy
 
 Run `tox` to run the test suite.
--- proxy/proxy/proxy.py
+++ proxy/proxy/proxy.py
@@ -1,7 +1,8 @@
 """
 Proxy requests to api.yr.no so that they can be accessed from web pages.
 
-To deploy, upload the contents of this file as a Google Cloud Function.
+To deploy, upload the contents of this file to Google Cloud Run:
+https://console.cloud.google.com/run/detail/europe-north1/api-met-no-proxy/source?inv=1&invt=Abq2CA&project=api-met-no-proxy
 """
 
 import json
