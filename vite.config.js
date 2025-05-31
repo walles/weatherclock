a3ff29f76389a591fc6dc603fed532e97f639aea
Johan Walles <johan.walles@gmail.com>
2025-05-31 15:51:03 +0200

    Verify formatting in CI

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/weatherclock/',
   plugins: [react()],
   build: {
-    outDir: 'build'
-  }
-})
+    outDir: 'build',
+  },
+});
