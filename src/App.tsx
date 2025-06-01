edeee91a4986d7cc0edeee92b34c1a80e4b305a8
Johan Walles <johan.walles@gmail.com>
2025-06-01 12:20:50 +0200

    WIP Add a toolbar
    Very ugly and place-holdery.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import PageVisibility from 'react-page-visibility';
 import Clock from './Clock';
 import TimeSelect, { NamedStartTime } from './TimeSelect';
+import MainToolbar from './MainToolbar';
 
 class App extends React.Component<{}, { startTime: NamedStartTime }> {
   constructor(props: {}) {
@@ -39,44 +40,52 @@ class App extends React.Component<{}, { startTime: NamedStartTime }> {
     const { startTime } = this.state;
     return (
       <PageVisibility onChange={this.handleVisibilityChange}>
-        <div className="App">
-          <div className="ClockAndButtons">
-            <TimeSelect daysFromNow={startTime.daysFromNow} onSetStartTime={this.onSetStartTime} />
+        <>
+          <MainToolbar />
+          <div className="App">
+            <div className="ClockAndButtons">
+              <TimeSelect
+                daysFromNow={startTime.daysFromNow}
+                onSetStartTime={this.onSetStartTime}
+              />
 
-            <Clock startTime={startTime} reload={this.setTimeToNow} />
+              <Clock startTime={startTime} reload={this.setTimeToNow} />
 
-            {/*
-            If you add a Weatherclock launcher to your home screen on an iPhone,
-            the page opened will not be in a web-browser (or at least look like
-            it's not).
+              {/*
+              If you add a Weatherclock launcher to your home screen on an iPhone,
+              the page opened will not be in a web-browser (or at least look like
+              it's not).
 
-            So we add a reload button of our own here.
-            */}
-            <button type="button" className="updateButton" onClick={this.setTimeToNow}>
-              Update forecast
-            </button>
+              So we add a reload button of our own here.
+              */}
+              <button type="button" className="updateButton" onClick={this.setTimeToNow}>
+                Update forecast
+              </button>
+            </div>
+
+            <p>
+              Weather forecast from <a href="yr.no">yr.no</a>, delivered by the{' '}
+              <a href="https://met.no/English/">Norwegian Meteorological Institute</a> and the{' '}
+              <a href="https://www.nrk.no/">NRK</a>. Northern lights forecasts based on{' '}
+              <a href="https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json">
+                NOAA&apos;s Planetary K Index forecast
+              </a>{' '}
+              together with{' '}
+              <a href="https://hjelp.yr.no/hc/en-us/articles/4411702484754-Aurora-forecast-on-Yr">
+                YR&apos;s interpretation thereof
+              </a>
+              .
+            </p>
+
+            <p>
+              <a
+                href={`https://github.com/walles/weatherclock/tree/${import.meta.env.VITE_GIT_SHA}`}
+              >
+                Source code on GitHub
+              </a>
+            </p>
           </div>
-
-          <p>
-            Weather forecast from <a href="yr.no">yr.no</a>, delivered by the{' '}
-            <a href="https://met.no/English/">Norwegian Meteorological Institute</a> and the{' '}
-            <a href="https://www.nrk.no/">NRK</a>. Northern lights forecasts based on{' '}
-            <a href="https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json">
-              NOAA&apos;s Planetary K Index forecast
-            </a>{' '}
-            together with{' '}
-            <a href="https://hjelp.yr.no/hc/en-us/articles/4411702484754-Aurora-forecast-on-Yr">
-              YR&apos;s interpretation thereof
-            </a>
-            .
-          </p>
-
-          <p>
-            <a href={`https://github.com/walles/weatherclock/tree/${import.meta.env.VITE_GIT_SHA}`}>
-              Source code on GitHub
-            </a>
-          </p>
-        </div>
+        </>
       </PageVisibility>
     );
   }
---
+++ src/MainToolbar.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import UpdateIcon from '@mui/icons-material/Update';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Typography from '@mui/material/Typography';
+
+const MainToolbar: React.FC = () => {
+  const [time, setTime] = React.useState(0);
+  const [menuAnchor, setMenuAnchor] = React.useState<null | HTMLElement>(null);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMenuAnchor(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setMenuAnchor(null);
+  };
+
+  return (
+    <AppBar position="static" color="default" elevation={1}>
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 0, marginRight: 2 }}>
+          WeatherClock
+        </Typography>
+        <Select
+          value={time}
+          onChange={(e) => setTime(Number(e.target.value))}
+          size="small"
+          sx={{ minWidth: 120, marginRight: 2 }}
+        >
+          <MenuItem value={0}>Now</MenuItem>
+          <MenuItem value={1}>Tomorrow</MenuItem>
+          <MenuItem value={2}>Monday</MenuItem>
+        </Select>
+        <IconButton color="primary" sx={{ marginRight: 2 }}>
+          <UpdateIcon />
+        </IconButton>
+        <IconButton color="inherit" onClick={handleMenuOpen}>
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MainToolbar;
